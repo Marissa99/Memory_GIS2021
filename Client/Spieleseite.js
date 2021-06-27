c8ebb1bbcfa59893e8b4b68f55dca6dc6c45a4c5
Marissa99 <reiser-marissa@t-online.de>
2021-06-27 20:43:57 +0200

    Spieleseite.ts mit Funktion aufdecken

---
+++ Client/Spieleseite.js
@@ -0,0 +1,20 @@
+"use strict";
+//EventListender für die Memory Karten
+let bilder = document.getElementsByClassName("Bild"); //auf Image umschreiben
+for (let i = 0; i < 16; i++) { //soll auf alle 16 Karten den Listener anwenden
+    bilder[i].addEventListener("click", bildAufdecken); //Zugriff auf das einzelen Element und Anwendung von Listener
+}
+let anzahlOffeneKarten = 0;
+console.log("Top");
+//Funktion Memory Karten aufdecken beim anklicken
+function bildAufdecken(_event) {
+    console.log("Top");
+    let geklicktesBild = _event.target; //Objekt welches geklickt wurde --> bekommmt Image
+    geklicktesBild.style.display = "block"; //oben schon in Image umgeschrieben
+    anzahlOffeneKarten = anzahlOffeneKarten + 1; //lokal Variable immer um 1 erhöhen um max 2 zu erfassen
+    console.log(anzahlOffeneKarten);
+    if (anzahlOffeneKarten == 2) {
+        anzahlOffeneKarten = 0;
+    }
+}
+//# sourceMappingURL=Spieleseite.js.map
---
+++ Client/Spieleseite.ts
@@ -0,0 +1,21 @@
+//EventListender für die Memory Karten
+let bilder: HTMLCollectionOf<HTMLImageElement> = <HTMLCollectionOf<HTMLImageElement>> document.getElementsByClassName("Bild"); //auf Image umschreiben
+for (let i: number = 0; i < 16 ; i++) { //soll auf alle 16 Karten den Listener anwenden
+    bilder[i].addEventListener("click", bildAufdecken); //Zugriff auf das einzelen Element und Anwendung von Listener
+}
+
+let anzahlOffeneKarten: number = 0;
+console.log("Top");
+
+//Funktion Memory Karten aufdecken beim anklicken
+function bildAufdecken(_event: Event): void {
+    console.log("Top");
+    let geklicktesBild: HTMLImageElement = <HTMLImageElement>_event.target; //Objekt welches geklickt wurde --> bekommmt Image
+    geklicktesBild.style.display = "block"; //oben schon in Image umgeschrieben
+    anzahlOffeneKarten =  anzahlOffeneKarten + 1 ; //lokal Variable immer um 1 erhöhen um max 2 zu erfassen
+    console.log (anzahlOffeneKarten);
+
+    if (anzahlOffeneKarten == 2) {
+        anzahlOffeneKarten = 0;
+    }
+}
