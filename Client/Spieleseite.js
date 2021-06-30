049f85366edbf00c7abbb3fec1159d417be3b4fb
Marissa99 <reiser-marissa@t-online.de>
2021-06-30 17:37:25 +0200

    MemorySpiel

--- Client/Spieleseite.js
+++ Client/Spieleseite.js
@@ -1,20 +1,48 @@
 "use strict";
-//EventListender für die Memory Karten
+//EventlistRueckender für die Memory Karten
 let rückseiten = document.getElementsByClassName("Rückseite"); //auf Image umschreiben
-for (let i = 0; i < 16; i++) { //soll auf alle 16 Karten den Listener anwenden
-    rückseiten[i].addEventListener("click", bildAufdecken); //Zugriff auf das einzelen Element und Anwendung von Listener
+for (let i = 0; i < 16; i++) { //soll auf alle 16 Karten den listRueckener anwenden
+    rückseiten[i].addEventListener("click", bildAufdecken); //Zugriff auf das einzelen Element und Anwendung von listRueckener
 }
-let anzahlOffeneKarten = 0;
 console.log("Top");
-//Funktion Memory Karten aufdecken beim anklicken
+let listRueck = []; //leeres Array um die umgedrehten Karten zu speichern
+let listFront = [];
+let dateTimeBegin;
+let dateTimeEnd;
+let pairs = 0;
+let clicks = 0;
+//Funktion Memory Karten aufdecken und vergleichen
 function bildAufdecken(_event) {
-    console.log("Top");
+    if (clicks == 0) { //um die Startzeit zu speichern
+        dateTimeBegin = new Date();
+        console.log(dateTimeBegin.getTime);
+    }
+    clicks = clicks + 1;
     let geklicktesBild = _event.target; //Objekt welches geklickt wurde --> bekommmt Image
-    geklicktesBild.style.display = "none"; //oben schon in Image umgeschrieben
-    anzahlOffeneKarten = anzahlOffeneKarten + 1; //lokal Variable immer um 1 erhöhen um max 2 zu erfassen
-    console.log(anzahlOffeneKarten);
-    if (anzahlOffeneKarten == 2) {
-        anzahlOffeneKarten = 0;
+    if (listRueck.length < 2) { //solange in der Liste der Rückseite noch nicht 2 Bilder vorhanden sind
+        let it = 0; //Laufvariable zum speichern
+        for (let item of rückseiten) { //collection der Rückseite durchgehen
+            if (item == geklicktesBild) { //wenn position in collection Rückseite = geklicktes Bild
+                let front = document.getElementsByClassName("Bild"); //holen der Collection Bilder
+                listFront.push(front[it]); //speichern in Liste
+            }
+            it++; //an welcher Stelle nacher eine Übereinstimmung gibt
+        }
+        listRueck.push(geklicktesBild); //Element is listRuecke speichern
+        geklicktesBild.style.visibility = "hidden"; //Bild ausblenden
+    }
+    //Timer um das umgedrehte "falsche" Bild anzuzeigen
+    if (listRueck.length == 2) { //wenn 2 Elemente in der listRuecke sind (geklickt sind)
+        if (listFront[0].src != listFront[1].src) { //wenn stelle 0 und 1 NICHT übereinstimmen      
+            listRueck.forEach(element => {
+                element.style.visibility = "visible"; //dann geklicktes Element sichtbar machen bzw. richtiges Bild  
+            });
+        }
+        listRueck = []; //listRueck leeren
+        listFront = []; //listFront leeren
+    }
+    if (pairs == 8) { //für die Zeit
+        dateTimeEnd = new Date();
     }
 }
 //# sourceMappingURL=Spieleseite.js.map
--- Client/Spieleseite.ts
+++ Client/Spieleseite.ts
@@ -1,32 +1,54 @@
-//EventListender für die Memory Karten
+//EventlistRueckender für die Memory Karten
 let rückseiten: HTMLCollectionOf<HTMLImageElement> = <HTMLCollectionOf<HTMLImageElement>> document.getElementsByClassName("Rückseite"); //auf Image umschreiben
-for (let i: number = 0; i < 16 ; i++) { //soll auf alle 16 Karten den Listener anwenden
-    rückseiten[i].addEventListener("click", bildAufdecken); //Zugriff auf das einzelen Element und Anwendung von Listener
+for (let i: number = 0; i < 16 ; i++) { //soll auf alle 16 Karten den listRueckener anwenden
+    rückseiten[i].addEventListener("click", bildAufdecken); //Zugriff auf das einzelen Element und Anwendung von listRueckener
 }
 
-let anzahlOffeneKarten: number = 0;
 console.log("Top");
+let listRueck: Array<HTMLImageElement> = []; //leeres Array um die umgedrehten Karten zu speichern
+let listFront: Array<HTMLImageElement> = [];
+let dateTimeBegin: Date;
+let dateTimeEnd: Date;
+let pairs: number = 0;
+let clicks: number = 0;
 
-//Funktion Memory Karten aufdecken beim anklicken
+//Funktion Memory Karten aufdecken und vergleichen
 
 function bildAufdecken(_event: Event): void {
-    console.log("Top");
-    if (anzahlOffeneKarten <= 2) {
-        let geklicktesBild: HTMLImageElement = <HTMLImageElement>_event.target; //Objekt welches geklickt wurde --> bekommmt Image
-        geklicktesBild.style.display = "none"; //oben schon in Image umgeschrieben
-        anzahlOffeneKarten =  anzahlOffeneKarten + 1 ; //lokal Variable immer um 1 erhöhen um max 2 zu erfassen
-        console.log (anzahlOffeneKarten);
+    if (clicks == 0) { //um die Startzeit zu speichern
+        dateTimeBegin = new Date();
+        console.log(dateTimeBegin.getTime);
     }
-
-    else if (anzahlOffeneKarten == 2) {
-        for (let i: number; i > 16; i++) {
-            .style.display = "block";
-             anzahlOffeneKarten = 0;
-
-            
-
-
+    clicks = clicks + 1;
+    
+    let geklicktesBild: HTMLImageElement = <HTMLImageElement>_event.target; //Objekt welches geklickt wurde --> bekommmt Image
+   
+    if (listRueck.length < 2) { //solange in der Liste der Rückseite noch nicht 2 Bilder vorhanden sind
+        let it: number = 0; //Laufvariable zum speichern
+        for (let item of rückseiten) { //collection der Rückseite durchgehen
+            if ( item == geklicktesBild ) { //wenn position in collection Rückseite = geklicktes Bild
+                let front: HTMLCollectionOf<HTMLImageElement> = <HTMLCollectionOf<HTMLImageElement>> document.getElementsByClassName("Bild"); //holen der Collection Bilder
+                listFront.push(front[it]); //speichern in Liste
+            }
+            it++; //an welcher Stelle nacher eine Übereinstimmung gibt
         }
-  
+        listRueck.push(geklicktesBild); //Element is listRuecke speichern
+        geklicktesBild.style.visibility = "hidden"; //Bild ausblenden
+        
+    }
+    //Timer um das umgedrehte "falsche" Bild anzuzeigen
+    
+    if (listRueck.length == 2) { //wenn 2 Elemente in der listRuecke sind (geklickt sind)
+        if (listFront[0].src != listFront[1].src) { //wenn stelle 0 und 1 NICHT übereinstimmen      
+            listRueck.forEach(element => { //über die listRueck drüber gehen 
+                element.style.visibility = "visible"; //dann geklicktes Element sichtbar machen bzw. richtiges Bild  
+            });  
+        }
+        listRueck = []; //listRueck leeren
+        listFront = []; //listFront leeren
+    }       
+    if (pairs == 8) { //für die Zeit
+        dateTimeEnd = new Date();
     }
 }
+
