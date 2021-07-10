de504c4712b671be4714b9125df3f4c0604753fb
Marissa99 <reiser-marissa@t-online.de>
2021-07-10 13:23:15 +0200

    nochmal alles lokal und test über gitHub

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ function serverStarten(_port) {
 //Funktion HandleRequest mit if Abfragen (welcher Pfadname) aufgerufen wird
 async function handleRequest(_request, _response) {
     console.log("Anfrage angekommen"); //Überprüfung ob Anfragen angekommen sind
-    _response.setHeader("content-type", "text/html; charset=utf-8"); //Eigenschaften von JSON "application/json; charset=utf-8"
+    _response.setHeader("content-type", "application/json; charset=utf-8"); //Eigenschaften von JSON
     _response.setHeader("Access-Control-Allow-Origin", "*"); //Zugriffserlaubnis: * alle dürfen darauf zugreifen
     if (_request.url) {
         let url = Url.parse(_request.url, true); //umwandlung query in assoziatives Array
